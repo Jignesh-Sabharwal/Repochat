40f2dcdff0536a4c418773d6e1245876c58b9181
Jignesh-Sabharwal <sabharwaljignesh@gmail.com>
2026-07-18 23:22:43 +0530

    Implement vectorstore and add chroma_db to gitignore

---
+++ vectorstore.py
@@ -0,0 +1,19 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+def build_vectorstore(chunks, persist_dir: str = "chroma_db"):
+    embeddings = HuggingFaceEmbeddings(model_name=EMBED_MODEL)
+    vectordb = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=persist_dir
+    )
+    return vectordb
+
+
+def load_vectorstore(persist_dir: str = "chroma_db"):
+    embeddings = HuggingFaceEmbeddings(model_name = EMBED_MODEL)
+    return Chroma(persist_directory = persist_dir, embedding_function = embeddings)
+    
